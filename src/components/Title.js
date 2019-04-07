cf8dfc647ce68eb15a4b60597439384fc92e653f
Danielle Dees <d.danielle83@gmail.com>
2019-04-06 20:53:03 -0400

    style

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function Title(props) {
   return  (<div className="title">
-  <h1>Clicky Game!</h1>
+  <h1>Clicky/Memory Game</h1>
   <p>Click on an image to earn points, but don't click on any more than once!</p>
   {props.children}
   </div>);
